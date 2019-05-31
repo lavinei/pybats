878fb5ac164b8aabb50c4a05337079a34d50d5ab
lavinei <lavine.isaac@gmail.com>
2019-05-31 11:54:35 -0400

    Changing Package Name

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="forecasting",
+    name="PyBATS",
     version="0.0.1",
     description="Bayesian Forecasting of Discrete Time Series",
     author="Isaac Lavine",
-    packages=['forecasting'],
-    package_dir={'forecasting': 'forecasting'},
-    package_data={'forecasting': ['pkg_data/*.pickle.gzip']}
+    author_email="lavine.isaac@gmail.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+    package_dir={'PyBATS': 'PyBATS'},
+    package_data={'PyBATS': ['pkg_data/*.pickle.gzip']}
 )
