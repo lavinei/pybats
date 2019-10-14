15ce02eaedcd9a7e66c63f70a90a894662f8f0e5
lavinei <lavine.isaac@gmail.com>
2019-10-14 16:47:41 -0400

    Improving example and readme

---
+++ build/lib/pybats/loss_functions.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+def MSE(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    return np.mean((y - f)**2)
+
+
+def MAD(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    return np.mean(np.abs(y-f))
+
+
+def MAPE(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    return 100*np.mean(np.abs((y - f)) / y)
+
+
+def WAPE(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    return 100*np.sum(np.abs(y-f)) / np.sum(y)
+
+
+def WAFE(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    return 100*np.sum(np.abs(y-f)) / ((np.sum(y) + np.sum(f))/2)
+
+
+def ZAPE(y, f):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    nonzeros = y.nonzero()[0]
+    n = len(y)
+    loss = np.copy(f)
+    loss[nonzeros] = np.abs(y[nonzeros] - f[nonzeros]) / y[nonzeros]
+    return 100*np.mean(loss)
+
+
+def scaledMSE(y, f, ymean = None):
+    y = np.ravel(y)
+    f = np.ravel(f)
+    if ymean is None:
+        # First check if the 'y' vector is longer than f
+        ny = len(y)
+        nf = len(f)
+        ymean = np.cumsum(y) / np.arange(1, ny+1)
+        # Assume that the forecasts and y vector terminate at the same point
+        y = y[-nf:]
+        ymean = ymean[-nf:]
+    return np.mean(((y.reshape(-1) - f.reshape(-1)) ** 2 / (ymean.reshape(-1) ** 2)))
--- examples/Poisson_DGLM.py
+++ examples/Poisson_DGLM.py
@@ -1,32 +1,34 @@
-import sys
-sys.path.insert(0,'../')
-
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from pybats.define_models import define_dglm
 from pybats.analysis import analysis
 from pybats.point_forecast import median
-from pybats.plot import plot_data_forecast
+from pybats.plot import plot_data_forecast, ax_style
+from pybats.shared import load_sales_example
 
-
-data = pd.read_csv('../data/sales.csv', index_col=0)
+data = load_sales_example()                                 # Load example sales and advertising data. Source: Abraham & Ledolter (1983)
 Y = data['Sales'].values
 X = data['Advertising'].values
 
-k = 1
-forecast_start = 15
-forecast_end = 35
-forecast_len = forecast_end + k - forecast_start
+k = 1                                                       # Forecast 1 step ahead
+forecast_start = 15                                         # Start forecast at time step 15
+forecast_end = 35                                           # End forecast at time step 35 (final time step)
 
-
+# Run a simple analysis
 mod, samples = analysis(Y, X, family="poisson",
-                        forecast_start = forecast_start, forecast_end=forecast_end, k = k,
-                        prior_length=6,
-                        rho = .5)
+                        forecast_start = forecast_start,    # First time step to forecast on
+                        forecast_end=forecast_end,          # Final time step to forecast on
+                        k = k,                              # Forecast horizon. If k>1, default is to forecast 1:k steps ahead, marginally
+                        prior_length=6,                     # How many data point to use in defining prior
+                        rho = .5,                           # Random effect extension, increases variance of Poisson DGLM (see Berry and West, 2019)
+                        deltrend=0.95,                      # Discount factor on the trend component (intercept)
+                        delregn=0.95                        # Discount factor on the regression component
+                        )
 
-forecast = median(samples)
+forecast = median(samples)                                  # Take the median as the point forecast
 
-fig, ax = plt.subplots(1,1)
-ax = plot_data_forecast(fig, ax, Y[forecast_start:forecast_end + k], forecast, samples, dates=np.arange(forecast_len))
+
+fig, ax = plt.subplots(1,1)                                 # Plot the 1-step ahead point forecast plus the 95% credible interval
+ax = plot_data_forecast(fig, ax, Y[forecast_start:forecast_end + k], forecast, samples,
+                        dates=np.arange(forecast_start, forecast_end+1, dtype='int'))
+ax = ax_style(ax, ylabel='Sales', xlabel='Time', xlim=[forecast_start, forecast_end])
 plt.savefig('./forecast.jpg')
