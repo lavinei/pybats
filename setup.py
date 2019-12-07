af85f1dc12167844f159e5ad0254e30c5680284d
lavinei <lavine.isaac@gmail.com>
2019-12-07 17:16:29 -0500

    updated setup description

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ setup(
     description="Bayesian Forecasting of Time Series",
     author="Isaac Lavine",
     author_email="lavine.isaac@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
+    url="https://lavinei.github.io/pybats/",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ], install_requires=['pandas', 'numpy', 'statsmodels', 'scipy'],
     package_dir={'pybats': 'pybats'},
-    package_data={'pybats': ['pkg_data/*']} #'pkg_data/*.pickle.gzip', 'pkg_data/sales.csv', 
+    package_data={'pybats': ['pkg_data/*']},
+    python_requires='>=3.6'#'pkg_data/*.pickle.gzip', 'pkg_data/sales.csv',
 )
